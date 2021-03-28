c65c8ece871e178479488386fc156eb92532aa0d
nurlasova <nurlasova.kg@gmail.com>
2021-03-28 15:08:11 +0600

    I added new project Build

---
+++ src/js/partners.js
@@ -0,0 +1,8 @@
+const swiper5 = new Swiper('.swiper5', {
+    // Optional parameters
+       loop: true,
+       slidesPerView:5,
+       slidesPerGroup:1,
+       spaceBetween:25,
+   
+})
---
+++ src/js/project.js
@@ -0,0 +1,12 @@
+const swiper1 = new Swiper('.swiper1', {
+    // Optional parameters
+        loop: true,
+        slidesPerView:3,
+        slidesPerGroup:1,
+        spaceBetween:25,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    });
