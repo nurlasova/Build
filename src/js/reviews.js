556a9a5ad8410968e1ec5120264c0db2f0b988b2
nurlasova <nurlasova.kg@gmail.com>
2021-03-28 15:57:13 +0600

    updated

---
+++ src/js/reviews.js
@@ -0,0 +1,10 @@
+// const swiper3 = new Swiper('.swiper3', {
+//     // Optional parameters
+//     direction: 'vertical',
+   
+//     slidesPerView:3,
+//     slidesPerGroup:1,
+//     spaceBetween:25,
+//     loop: true,
+  
+//   });
